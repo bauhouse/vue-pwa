d7d50a2d81ce8feaac58848b0cce543ec4b2edd2
Glitch (vue-pwa) <none>
2019-06-08 19:22:14 +0000

    Component Basics: blog posts

--- public/client.js
+++ public/client.js
@@ -83,3 +83,20 @@ Vue.component('button-counter', {
 })
 
 new Vue({ el: '#components-demo' })
+
+// Define a new component called blog-post
+Vue.component('blog-post', {
+  props: ['title'],
+  template: '<h3>{{ title }}</h3>'
+})
+
+new Vue({
+  el: '#blog-post-demo',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue' },
+      { id: 2, title: 'Blogging with Vue' },
+      { id: 3, title: 'Why Vue is so fun' }
+    ]
+  }
+})
